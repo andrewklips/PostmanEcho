fc6ebc6b8a1840fb474ec91ffcbf1c84e8626114
andrewklips <andrew.klips@gmail.com>
2022-09-20 19:22:51 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/wao2fynarwfeguax?svg=true)](https://ci.appveyor.com/project/andrewklips/api-2)
---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,25 @@
+package ru.netology;
+
+import io.restassured.response.ValidatableResponse;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    public void postmanEchoTest() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some value")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data",equalTo("some value"));
+
+
+    }
+
+}
