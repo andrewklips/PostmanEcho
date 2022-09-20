4003451c50bdc92cd87bd80566785898ae32aea7
andrewklips <andrew.klips@gmail.com>
2022-09-20 19:30:59 +0500

    test fail

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -12,12 +12,12 @@ public class PostmanEchoTest {
         given()
                 .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8")
-                .body("some value")
+                .body("some data")
                 .when()
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data",equalTo("some value"));
+                .body("name",equalTo("some data"));
 
 
     }
